2bc180db9784513a94ab05a5058a79ca48ca0b99
elenaf007 <96072892+elenaf007@users.noreply.github.com>
2024-01-06 15:37:05 -0600

    fix: formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -259,8 +259,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const minutesStr = minutes.toString().padStart(2, '0');
+  const secondsStr = seconds.toString().padStart(2, '0');
+  return minutesStr.concat(':', secondsStr);
 }
 
 /**
